ef46f8445af7fee861c00dadab9d7ff718c00392
Lucas Santana Lemos <lslemos59@gmail.com>
2024-02-23 16:15:58 -0300

    #

--- library/models.py
+++ library/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=55)
+    slug = models.SlugField(unique=True, default='')
+    author = models.CharField(max_length=55)
+    price = models.FloatField()
+    cover = models.ImageField(upload_to='library/covers/%Y/%m/%d/', blank=True, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    avaliable_until = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, default=None)
--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 from library import views
+from project import settings
+from django.conf.urls.static import static
 
 app_name = 'library'
 
@@ -9,3 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
