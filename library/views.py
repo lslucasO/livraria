1510be358cb876f7b919890061d8ae762b2183b6
Lucas Santana Lemos <lslemos59@gmail.com>
2024-02-23 10:20:03 -0300

    #

--- library/views.py
+++ library/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import *
 
 def home(request):
-    return render(request, '')
+    return render(request, 'global/partials/home.html')
