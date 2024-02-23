6df2c4b7e9e69f54c50edfe527c42f710ce2ee92
Lucas Santana Lemos <lslemos59@gmail.com>
2024-02-23 10:23:44 -0300

    #

--- library/views.py
+++ library/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import *
 
 def home(request):
-    return render(request, 'global/partials/home.html')
+    context = {
+        'title': 'Home'
+    }
+    
+    return render(request, 'global/partials/home.html', context)
