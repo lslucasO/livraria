a4ab97509f6d68c5dc09abe748eced70ef706bc6
Lucas Santana Lemos <lslemos59@gmail.com>
2024-02-24 10:13:45 -0300

    #

--- project/settings.py
+++ project/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'bc4C2bgcbEgc1d*FF3*-*E-c1*eeGd-A',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '16103',
     }
 }
 
