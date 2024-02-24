d8b5d393a5731b081ddf3fab2ba77eac0c28833c
Lucas Santana Lemos <lslemos59@gmail.com>
2024-02-24 11:34:15 -0300

    #

--- project/settings.py
+++ project/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
