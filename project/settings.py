08e1defd01c673ae803e0d5a08952dca282fbe4a
Lucas Santana Lemos <lslemos59@gmail.com>
2024-02-24 10:53:27 -0300

    #

--- project/settings.py
+++ project/settings.py
@@ -134,7 +134,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'base_static'
+    BASE_DIR / 'base_static',
+    BASE_DIR / 'media',
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
