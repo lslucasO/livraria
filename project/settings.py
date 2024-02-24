f9e2df705f8a1498a643a44f621717c116ae96c9
Lucas Santana Lemos <lslemos59@gmail.com>
2024-02-24 10:26:27 -0300

    #

--- project/settings.py
+++ project/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'base_static'
 ]
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
