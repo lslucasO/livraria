5288d11d98032dfdf9589090ce428ff39cb5bacf
Lucas Santana Lemos <lslemos59@gmail.com>
2024-02-24 22:51:25 -0300

    #

--- library/models.py
+++ library/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+
 class Book(models.Model):
     title = models.CharField(max_length=55)
     slug = models.SlugField(unique=True, default='')
@@ -10,8 +12,12 @@ class Book(models.Model):
     cover = models.ImageField(upload_to='covers/', blank=True, default='')
     created_at = models.DateTimeField(auto_now_add=True)
     avaliable_until = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=False)
     synopsis = models.TextField(max_length=1000, default='')
+    language = models.CharField(max_length=30, default='')
+    number_of_pages = models.IntegerField(default=0)
+    edition = models.IntegerField(default=0)
+    cape = models.CharField(default='')
+    is_published = models.BooleanField(default=False)
     
     creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, default=None)
 
