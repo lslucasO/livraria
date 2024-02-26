ff008486c27c94ec2ae879c549f99a86211f32df
Lucas Santana Lemos <lslemos59@gmail.com>
2024-02-26 18:29:54 -0300

    #

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Profile(models.Model):
     author = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_icon = models.ImageField(upload_to='profiles/icons/', blank=True, default='')
--- users/views.py
+++ users/views.py
@@ -1,12 +1,29 @@
+from django.http import Http404
 from django.shortcuts import render
+from django.urls import reverse
 from .forms import RegisterForm
 
+
 def register_view(request):
-    form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     
     context = {
         'title': 'Criar Conta',
-        'form': form
+        'form': form,
+        'form_action': reverse(register_create)
     }
     
-    return render(request, 'users/pages/create-account-page.html', context)
+    return render(request, 'users/pages/register.html', context)
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+    
+    POST = request.POST 
+    # Salvando os dados do usuario por sessão
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+    
+    return render(request, '')
