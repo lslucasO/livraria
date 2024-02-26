968b529b3dfcc7be520efdf13ee5b441849e0ace
Lucas Santana Lemos <lslemos59@gmail.com>
2024-02-26 18:12:45 -0300

    #

--- users/models.py
+++ users/models.py
@@ -4,4 +4,5 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     author = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_icon = models.ImageField(upload_to='profiles/icons/', blank=True, default='')
     bio = models.TextField(default='', blank=True)
