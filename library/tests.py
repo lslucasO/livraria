9f9de462038b4a196c29efcb0f78775c065f86c8
Lucas Santana Lemos <lslemos59@gmail.com>
2024-02-28 18:25:55 -0300

    #

--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Essa classe deriva todos os testes da aplicação
+class LibraryURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        ...
+    
