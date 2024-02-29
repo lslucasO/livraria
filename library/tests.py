fd2f3abfa5b0aaa47defd636bef985f258694392
Lucas Santana Lemos <lslemos59@gmail.com>
2024-02-29 16:41:25 -0300

    #

--- library/tests.py
+++ library/tests.py
@@ -3,5 +3,6 @@ from django.test import TestCase
 # Essa classe deriva todos os testes da aplicação
 class LibraryURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
-        ...
+        variavel = '123456'
+        assert 1 == 2, 'Um é igual a dois'
     
